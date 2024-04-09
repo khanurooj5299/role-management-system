9f88d4b33c11d42fbe285bc3375174738dea66d7
urooj khan <khanurooj5299@gmail.com>
2024-04-09 21:30:27 +0530

    Worked on LoginComponent

--- src/app/Auth/components/login/login.component.ts
+++ src/app/Auth/components/login/login.component.ts
@@ -1,13 +1,45 @@
 import { Component } from '@angular/core';
-import {MatFormField, MatInputModule} from '@angular/material/input';
+import { MatFormField, MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { NgIf } from '@angular/common';
+import { AuthService } from '../../services/auth.service';
+import { LoginResponseModel } from '../../models/login-response.model';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [MatInputModule, MatFormField],
+  imports: [
+    MatInputModule,
+    MatFormField,
+    MatCardModule,
+    MatDividerModule,
+    MatButtonModule,
+    FormsModule,
+    MatIconModule,
+    NgIf,
+  ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
-export class LoginComponent {
+export class LoginComponent{
+  hide = true;
+  email: string = '';
+  password: string = '';
 
+  constructor(private authService: AuthService) {}
+
+  onLogin() {
+    this.authService.login({email: this.email, password: this.password}).subscribe({
+      next: (res: LoginResponseModel)=>{
+
+      },
+      error: (err)=>{
+        
+      }
+    });
+  }
 }
