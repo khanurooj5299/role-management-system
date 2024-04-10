9ba7d28e7ce6906098e3f18c0d18fdc211bc52b9
urooj khan <khanurooj5299@gmail.com>
2024-04-10 09:50:23 +0530

    Created User list component

--- src/app/Admin/components/admin-dashboard/admin-dashboard.component.ts
+++ src/app/Admin/components/admin-dashboard/admin-dashboard.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { UserListComponent } from '../user-list/user-list.component';
+import { MatButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-admin-dashboard',
   standalone: true,
-  imports: [],
+  imports: [UserListComponent, MatButton],
   templateUrl: './admin-dashboard.component.html',
   styleUrl: './admin-dashboard.component.css'
 })
