9e1bd0a7022f8389ecca2c6f94870946a6a97698
urooj khan <khanurooj5299@gmail.com>
2024-04-10 06:10:16 +0530

    Implemneted logout in header

--- src/app/Admin/components/admin-header/admin-header.component.ts
+++ src/app/Admin/components/admin-header/admin-header.component.ts
@@ -2,14 +2,20 @@ import { Component } from '@angular/core';
 import { MatButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
+import { AuthService } from '../../../Auth/services/auth.service';
 
 @Component({
   selector: 'app-admin-header',
   standalone: true,
   imports: [MatIcon, MatButton, RouterLink],
   templateUrl: './admin-header.component.html',
-  styleUrl: './admin-header.component.css'
+  styleUrl: './admin-header.component.css',
 })
 export class AdminHeaderComponent {
 
+  constructor(private authService: AuthService) {}
+  
+  onLogout() {
+    this.authService.logout();
+  }
 }
--- src/app/Auth/services/auth.service.ts
+++ src/app/Auth/services/auth.service.ts
@@ -4,12 +4,13 @@ import { environment } from '../../../environments/environment.development';
 
 import { LoginRequestModel } from '../models/login-request.model';
 import { LoginResponseModel } from '../models/login-response.model';
+import { Router } from '@angular/router';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
   private apiUrl = environment.apiUrl;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private router: Router) {}
 
   login(authModel: LoginRequestModel) {
     return this.http.post<LoginResponseModel>(
@@ -18,9 +19,19 @@ export class AuthService {
     );
   }
 
+  logout() {
+    this.deleteUserandToken();
+    this.router.navigate(['auth/login']);
+  }
+
   setUserAndToken(loginResponse: LoginResponseModel) {
     localStorage.setItem('user', JSON.stringify(loginResponse.user));
     localStorage.setItem('token', loginResponse.token);
+  }
+
+  deleteUserandToken() {
+    localStorage.removeItem('user');
+    localStorage.removeItem('token');
   }
 
   getToken() {
