1fad1854f58300d6af73f00071e363301a60864b
urooj khan <khanurooj5299@gmail.com>
2024-04-11 13:23:30 +0530

    Removed datasource from user-list Added logic to confirm component

--- src/app/Admin/components/user-list/user-list.component.ts
+++ src/app/Admin/components/user-list/user-list.component.ts
@@ -1,18 +1,22 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { AdminService } from '../../services/admin.service';
-import { UserModel } from '../../../Shared/models/user.model';
+import { Component, OnInit } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
 import { MatCardModule } from '@angular/material/card';
 import { MatDivider } from '@angular/material/divider';
-import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatTableModule } from '@angular/material/table';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faEye, faTrash, faUserEdit } from '@fortawesome/free-solid-svg-icons';
+import { Router, RouterModule } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
 import {
-  MatPaginator,
   MatPaginatorModule,
   PageEvent,
 } from '@angular/material/paginator';
-import { Router, RouterModule } from '@angular/router';
+
+import { UserModel } from '../../../Shared/models/user.model';
+import { AdminService } from '../../services/admin.service';
 import { UserManagementService } from '../../services/user-management.service';
+import { ConfirmDialogComponent } from '../../../Shared/components/confirm-dialog/confirm-dialog.component';
+import { SnackbarService } from '../../../Shared/services/snackbar.service';
 
 @Component({
   selector: 'app-user-list',
@@ -29,7 +33,7 @@ import { UserManagementService } from '../../services/user-management.service';
   styleUrl: './user-list.component.css',
 })
 export class UserListComponent implements OnInit {
-  users: UserModel[] = [];
+  users!: UserModel[];
   faEye = faEye;
   faUserEdit = faUserEdit;
   faTrash = faTrash;
@@ -37,27 +41,32 @@ export class UserListComponent implements OnInit {
   pageIndex = 0;
   totalUserCount = 0;
   displayedColumns: string[] = ['name', 'email', 'role', 'actions'];
-  dataSource = new MatTableDataSource<UserModel>([]);
-  @ViewChild(MatPaginator) paginator!: MatPaginator;
 
   constructor(
     private adminService: AdminService,
     private router: Router,
-    private userManagementService: UserManagementService
+    private userManagementService: UserManagementService,
+    private dialog: MatDialog,
+    private snackbarService: SnackbarService
   ) {}
 
   ngOnInit(): void {
     this.getUserCount();
   }
 
-  ngAfterViewInit() {
-    this.dataSource.paginator = this.paginator;
-  }
-
   getUserCount() {
     this.adminService.getUserCount().subscribe({
       next: (res) => {
         this.totalUserCount = res.count;
+        this.getUsers();
+      },
+      error: (err: HttpErrorResponse) => {
+        this.snackbarService.show(
+          'center',
+          'top',
+          'Total user count not available!'
+        );
+        //still try to load the users
         this.getUsers();
       },
     });
@@ -68,9 +77,19 @@ export class UserListComponent implements OnInit {
       next: (res: UserModel[]) => {
         this.users = res;
       },
+      error: (err: HttpErrorResponse) => {
+        this.snackbarService.show(
+          'center',
+          'top',
+          err.error + " Please try again later!"
+        );
+      },
     });
   }
 
+  //remember that we don't need to use a MatTableDataSource because that is required for 
+  //frontend pagination i-e., when we have all data avilable on frontend and want to paginate
+  //here we are using backend pagination
   onPage(event: PageEvent) {
     this.pageIndex = event.pageIndex;
     this.pageSize = event.pageSize;
@@ -78,13 +97,31 @@ export class UserListComponent implements OnInit {
   }
 
   onUserManage(mode: 'view' | 'edit', user: UserModel) {
-    this.userManagementService.setUser({...user});
+    this.userManagementService.setUser({ ...user });
     this.router.navigate(['admin/user-management'], {
       queryParams: { mode },
     });
   }
 
-  onDelete(id: string) {
-    
+  onDelete(id: string, rowIndex: number) {
+    // const dialogRef = this.dialog.open(ConfirmDialogComponent, {
+    //   data: 'Are you sure you want to delete this user?',
+    // });
+    // dialogRef.afterClosed().subscribe((confirmed) => {
+    //   if (confirmed) {
+    //     this.adminService.deleteUser(id).subscribe({
+    //       next: (res) => {
+    //         this.snackbarService.show('right', 'top', res.message);
+    //         //we dont fetch data from backend again but update the frontend state only
+    //         this.totalUserCount--;
+    //         this.dataSource = new MatTableDataSource(this.dataSource.data.splice(rowIndex, 1));
+    //         this.dataSource.paginator = this.paginator;
+    //       },
+    //       error: (err: HttpErrorResponse) => {
+    //         this.snackbarService.show('center', 'top', err.error);
+    //       },
+    //     });
+    //   }
+    // });
   }
 }
--- src/app/Shared/components/confirm-dialog/confirm-dialog.component.ts
+++ src/app/Shared/components/confirm-dialog/confirm-dialog.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import {
+  MAT_DIALOG_DATA,
+  MatDialogContent,
+  MatDialogTitle,
+  MatDialogActions,
+  MatDialogClose,
+} from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-confirm-dialog',
   standalone: true,
-  imports: [],
+  imports: [
+    MatDialogContent,
+    MatDialogTitle,
+    MatDialogActions,
+    MatDialogClose,
+    MatButtonModule,
+  ],
   templateUrl: './confirm-dialog.component.html',
-  styleUrl: './confirm-dialog.component.css'
+  styleUrl: './confirm-dialog.component.css',
 })
 export class ConfirmDialogComponent {
-
+  title: string;
+  constructor(@Inject(MAT_DIALOG_DATA) title: string) {
+    this.title = title ? title : 'Are you sure?';
+  }
 }
